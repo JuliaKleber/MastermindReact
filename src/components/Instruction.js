c718c2f067bffa32926a8bebeca1fe0505e258a1
Julia Kleber <julia.kleber@gmx.net>
2024-01-13 16:58:21 +0100

    Revert "Merge pull request #2 from JuliaKleber/gh-pages"
    This reverts commit 34dcc93a28fb96db3461bc9fc899aa0f56d37a15, reversing
    changes made to f094993d530afdff8f6b5a8d1e51f28a780c3b58.

---
+++ src/components/Instruction.js
@@ -0,0 +1,26 @@
+// An instruction for the game is shown.
+
+import React from "react";
+
+const Instruction = (props) => {
+  return (
+    <div className="card big-card" id="instruction">
+      <h3>Spielanleitung für Mastermind</h3>
+      <p>
+        Der Computer wählt zufällig {props.numberInputFields} Farben aus. Ziel
+        ist es, diese Farben zu erraten. Farben können mehrfach in der Lösung
+        vorkommen.
+      </p>
+      <p>
+        Zum Setzen der Farben wähle eines der {props.countColors} Farbfelder
+        an. Dann kannst du die weißen Quadrate der Versuche einfärben, indem du
+        sie auswählst.
+      </p>
+      <p>
+        Du hast insgesamt {props.numberTrials} Versuche um die Lösung zu finden.
+      </p>
+    </div>
+  );
+}
+
+export default Instruction;
---
+++ src/components/OutputSolutionFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OutputSolutionFound = () => {
+  return (
+    <p className="centered-text">
+      <b>Herzlichen Glückwunsch!</b> <br />
+      Du hast die richtige Lösung gefunden.
+    </p>
+  );
+}
+
+export default OutputSolutionFound;
